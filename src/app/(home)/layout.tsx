11ab87e837497bfdecff2251745e45d858ef2b73
byhow <by.how@hotmail.com>
2024-06-14 20:52:38 -0700

    feat: add button placeholder and change styling

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,14 +2,16 @@ import ConnectWalletButton from "@/components/rainbow-button";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col text-[15px] leading-[18px] md:px-20 md:py-2">
+    <div className="flex flex-col text-[15px] leading-[18px] md:px-20 md:py-2">
       <header className="flex items-center justify-end gap-2 bg-emerald-300 px-1 py-2 pr-2 text-sm md:py-1">
         <div className="flex min-w-[30%] flex-col items-end md:min-w-[inherit] md:flex-row md:items-center my-2 mr-2">
           <ConnectWalletButton label="Connect" />
         </div>
       </header>
 
-      <main className=" bg-[#f6f6ef] px-1 py-4 md:px-2">{children}</main>
+      <main className="bg-white dark:bg-slate-800 px-1 py-4 md:px-2">
+        {children}
+      </main>
     </div>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <p>yo</p>
+      <button className="border border-black px-2 py-2">
+        1 WETH -&gt; 1 ETH
+      </button>
+      <br />
+      <button className="border border-black px-2 py-2">
+        1 ETH -&gt; 1 WETH
+      </button>
     </main>
   );
 }
