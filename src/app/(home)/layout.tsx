b86bbe8ad1bf5d6d2975e6aaf234f5408e180026
byhow <by.how@hotmail.com>
2024-06-17 15:46:44 -0700

    feat: add back to home button

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,9 +1,13 @@
 import ConnectWalletButton from "@/components/Button/ConnectWallet";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col text-[15px] leading-[18px] md:px-20 md:py-2">
-      <header className="flex items-center justify-end gap-2 bg-emerald-300 px-1 py-2 pr-2 text-sm md:py-1">
+      <header className="flex items-center justify-between gap-2 bg-emerald-300 px-1 py-2 pr-2 text-sm md:py-1">
+        <Link href="/" className="px-3 py-2 font-bold text-lg">
+          WETH on Arbitrum
+        </Link>
         <div className="flex min-w-[30%] flex-col items-end md:min-w-[inherit] md:flex-row md:items-center my-2 mr-2">
           <ConnectWalletButton label="Connect" />
         </div>
